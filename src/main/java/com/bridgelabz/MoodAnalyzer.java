7525426a2cfd9b046f2b2a50d9856345f321627b
kamal14rajput <kamal14rajput@gmail.com>
2022-01-07 01:07:48 +0530

    UPDATED: handle exception if user provide invalid mood

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -2,9 +2,30 @@ package com.bridgelabz;
 
 public class MoodAnalyzer {
 
+	public String message;
+
 	public String analyseMood(String message) {
-		if(message.contains("sad"))
+		if (message.contains("Sad"))
 			return "SAD";
 		return "HAPPY";
 	}
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public MoodAnalyzer() {
+		this.message = message;
+	}
+
+	public String exceptionAnalyseMood() {
+		try {
+			if (message.contains("sad"))
+				return "SAD";
+			return "HAPPY";
+		} catch (NullPointerException e) {
+			return "HAPPY";
+		}
+	}
 }
+
